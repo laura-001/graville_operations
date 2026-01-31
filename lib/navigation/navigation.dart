07ecb989cd9d0e8117e619d296b3979944db2980
piexie3 <emmanuelkbett3@gmail.com>
2026-01-31 12:22:59 +0300

    updated navigation widget

---
+++ lib/navigation/navigation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatelessWidget {
+    const Navigation({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('Navigation'),
+            ),
+            body: const Center(
+                child: Text('Navigation Widget'),
+            ),
+        );
+    }
+}
